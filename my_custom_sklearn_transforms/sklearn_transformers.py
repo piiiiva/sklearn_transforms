bf410c0dced53e2ce4d21c3891a7d0c21b0f3471
piiiiva <piiiiva@gmail.com>
2020-08-22 02:47:10 -0300

    Add new classes

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -13,12 +13,11 @@ class DropColumns(BaseEstimator, TransformerMixin):
 
     def transform(self, X):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
-        data = X.copy()
+        df = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
-        return data.drop(labels=self.columns, axis='columns')
+        return df.drop(labels=self.columns, axis='columns')
 
-# All sklearn Transforms must have the `transform` and `fit` methods
-class MediaNotas(BaseEstimator, TransformerMixin):
+class AddMediaNotasExatas(BaseEstimator, TransformerMixin):
   '''Recebe uma string para ser o nome da coluna de médias'''
 
   def __init__(self, nome_da_coluna):
@@ -29,17 +28,68 @@ class MediaNotas(BaseEstimator, TransformerMixin):
 
   def media_notas(self, x):
     notas = []
-    notas.append(x['NOTA_DE']) if x['NOTA_DE'] != np.nan else notas.append[0]
-    notas.append(x['NOTA_EM']) if x['NOTA_EM'] != np.nan else notas.append[0]
-    notas.append(x['NOTA_MF']) if x['NOTA_MF'] != np.nan else notas.append[0]
-    notas.append(x['NOTA_GO']) if x['NOTA_GO'] != np.nan else notas.append[0]
-    media = np.sum(notas)/4
+    notas.append(x['NOTA_MF'])
+    notas.append(x['NOTA_GO'])
+
+    media = np.mean(notas) 
     return pd.Series(data=[media], index=[self.nome])
 
-  # Para transformar nunca vai o y
   def transform(self, X):
-    data = X.copy()
+    df2 = X.copy()
 
-    coluna_media = data.apply(self.media_notas, axis=1)
-    data_com_media = data.join(coluna_media)
-    return data_com_media
+    coluna_media_exatas = df2.apply(self.media_notas, axis=1)
+    df2 = df2.join(coluna_media_exatas)
+    return df2
+
+class AddNotasHumanas(BaseEstimator, TransformerMixin):
+  '''Recebe uma string para ser o nome da coluna de médias'''
+
+  def __init__(self, nome_da_coluna):
+    self.nome = nome_da_coluna
+
+  def fit(self, X, y=None):
+    return self
+
+  def media_notas(x):
+    notas = []
+    notas.append(x['NOTA_DE'])
+    notas.append(x['NOTA_EM'])
+
+    media = np.mean(notas) 
+
+    return pd.Series(data=[media], index=[self.nome])
+
+  def transform(self, X):
+    df2 = X.copy()
+
+    coluna_media_humanas = df2.apply(self.media_notas, axis=1)
+    df2 = df2.join(coluna_media_humanas)
+    return df2
+
+All sklearn Transforms must have the `transform` and `fit` methods
+class AddNotasGeral(BaseEstimator, TransformerMixin):
+  '''Recebe uma string para ser o nome da coluna de médias'''
+
+  def __init__(self, nome_da_coluna):
+    self.nome = nome_da_coluna
+
+  def fit(self, X, y=None):
+    return self
+
+  def media_notas(x):
+    notas = []
+    notas.append(x['NOTA_DE'])
+    notas.append(x['NOTA_EM'])
+    notas.append(x['NOTA_MF'])
+    notas.append(x['NOTA_GO'])
+
+    media = np.mean(notas) 
+
+    return pd.Series(data=[media], index=[self.nome])
+
+  def transform(self, X):
+    df2 = X.copy()
+
+    coluna_media_geral = df2.apply(self.media_notas, axis=1)
+    df2 = df2.join(coluna_media_geral)
+    return df2
